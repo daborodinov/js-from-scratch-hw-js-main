025c4239a93cc607f2e2ad745018f4fb70c26b5b
daborodinov <daborodinov@yandex.ru>
2026-04-06 20:55:58 +0700

    complete lesson-04-04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,14 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+
+function doubleEachCharacter(string) {
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+        const element = string[i];
+        result += element + element
+    }
+    return result
+}
+
+console.log(doubleEachCharacter('Daniel'));
