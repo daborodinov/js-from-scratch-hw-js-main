5e552ed4dc98b7797b259a680e02f4df001942c2
daborodinov <daborodinov@yandex.ru>
2026-04-06 21:04:50 +0700

    complete lesson-04-01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -14,9 +14,10 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
 function includesElement(array, element) {
+
     for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        if (array === element) {
+        const current = array[i];
+        if (current === element) {
             return true
         }
         else return false
