a965f403bb0bc195f5a5a10061d0e3fb47c2136f
daborodinov <daborodinov@yandex.ru>
2026-04-06 20:14:28 +0700

    complete lesson-04-01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,14 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (array === element) {
+            return true
+        }
+        else return false
+    }
+}
+
+console.log(includesElement([1, 2, 3], 2))
